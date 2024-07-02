1779fd1bc33efc94cde6166e70d7e14882db4990
e <jannatul0040@gmail.com>
2024-07-02 08:19:30 +0600

    fixed

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import IntroductionSection from './components/IntroductionSection'
 import ConsultationModal from './components/ConsultationModal'
+import SectionWrapper from '@/partials/sections/Section/SectionWrapper'
+import FlexSection from '@/partials/sections/Section/FlexSection'
 
 function page() {
   return (
-    <section>
+    <SectionWrapper extraClass={"pt-0 pb-0"}>
       <IntroductionSection />
       <ConsultationModal />
-    </section>
+    </SectionWrapper>
   )
 }
 
